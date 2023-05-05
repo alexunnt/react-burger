73015fac250b4bc1b7f6b23c3a3470f85a0e1a79
Aleksei Bulgakov <contact@alexbulgakov.ru>
2023-05-06 01:28:25 +0700

    added main components: header, burger constructor and ingredients

---
+++ src/components/app/app.js
@@ -0,0 +1,19 @@
+import AppHeader from '../app-header/app-header';
+import BurgerConstructor from '../burger-constructor/burger-constructor';
+import BurgerIngredients from '../burger-ingredients/burger-ingredients';
+
+import appStyles from './app.module.css';
+
+function App() {
+    return (
+        <div className={appStyles.app}>
+            <AppHeader />
+            <div className={appStyles.burgerConstructor}>
+                <BurgerIngredients />
+                <BurgerConstructor />
+            </div>
+        </div >
+    );
+}
+
+export default App;
